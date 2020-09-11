769783e12b89a8f0b8d6432a4121919359c89d00
Alex Arno <alex.arno@Mac195.local>
2020-09-11 09:31:47 -0300

    ajustes

--- src/modules/orders/services/FindOrderService.ts
+++ src/modules/orders/services/FindOrderService.ts
@@ -1,7 +1,5 @@
 import { inject, injectable } from 'tsyringe';
 
-// import IProductsRepository from '@modules/products/repositories/IProductsRepository';
-// import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
 
@@ -14,8 +12,6 @@ class FindOrderService {
   constructor(
     @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
-    // private productsRepository: IProductsRepository,
-    // private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ id }: IRequest): Promise<Order | undefined> {
--- src/shared/infra/typeorm/migrations/1599767918879-CreateOrdersProducts.ts
+++ src/shared/infra/typeorm/migrations/1599767918879-CreateOrdersProducts.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
 
 export class CreateOrdersProducts1599767918879 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
